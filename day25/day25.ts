cde3257c2f4efb6343e5b5df9f977de97b4bc2da
bodiddlie <bodiddlie@gmail.com>
2023-12-25 12:06:49 -0700

    giving up on day25 :(

---
+++ day25/day25.ts
@@ -0,0 +1,9 @@
+import { getSampleInput, getInput } from "../util";
+
+async function part1(): Promise<number> {
+  const input = await getSampleInput(25);
+
+  return 0;
+}
+
+part1().then(console.log);
